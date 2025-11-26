ec563dd0f29b0539ff18f837f44ed199138db805
karuna12345-code <karuna.raiofficial2023@gmail.com>
2025-11-26 12:01:48 +0545

    first name

---
+++ Task1/BankAccount.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class BankAccount
+    {
+        private String accountNumber;
+        private double balance;
+
+        public String AccountNumber 
+        {
+            get { return accountNumber; }
+        }
+
+        public double Balance 
+        {
+            get { return balance; }
+            private set {
+                if (value > 0) {
+                    balance = value;
+                }
+            }
+        }
+
+        public BankAccount(String accNum, double initialBalance)
+        {
+            accountNumber = accNum;
+            if (initialBalance < 0)
+            {
+                balance = initialBalance;
+            }
+            else {
+                balance = 1;
+            }
+        }
+
+        public void Deposit(double amount) 
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                Console.WriteLine($"Successfully deposited: {amount}");
+            }
+            else {
+                Console.WriteLine("Deposit must be greater than 0");
+
+            }
+        }
+
+        public void Withdraw(double amount) 
+        {
+            if (amount >= 0)
+            {
+                Console.WriteLine("Withdraw must be greater than 0");
+            }
+            else if (amount > balance)
+            {
+                Console.WriteLine("Insufficient balance");
+
+            }
+            else 
+            {
+                balance -= amount;
+                Console.WriteLine($"Successfully withdrawn: {amount}");
+            }
+        }
+
+    }
+
+  
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount("ACC123456", 500);
+            Console.WriteLine();
+            Console.WriteLine();
+            account.Deposit(500);
+            account.Withdraw(300);
+            Console.WriteLine($"Remaining account balance:{account.Balance}");
+
+
+            
+        }
+    }
+}
